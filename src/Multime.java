b71cf203284a7419170a6031564121ccdd7f083d
Cristian Lucian IOAN <44252711+cristian-ioan@users.noreply.github.com>
2018-10-19 21:16:31 +0300

    Add files via upload

--- src/Multime.java
+++ src/Multime.java
@@ -1,66 +1,1 @@
-public class Multime {
-
-    private int[] date;
-    private int dim;
-    private int n;
-
-    public Multime(){
-        this.date = new int[0];
-        this.dim = 10;
-        this.n = 0;
-    }
-
-    public Multime(int dim){
-        this.date = new int[dim];
-        this.dim = dim;
-        this.n = 0;
-    }
-
-    public void adauga(int elem){
-        if (n != dim){
-            boolean found = false;
-            for (int i = 0; i < dim; i++) {
-                if (date[i] == elem){
-                    found = true;
-                }
-            }
-            int i = 0;
-            while (i < dim && found == false){
-                if (date[i] == 0){
-                    date[i] = elem;
-                    n++;
-                    found = true;
-                }
-                i++;
-            }
-        } else {
-            System.out.println("Vectorul este plin.");
-        }
-    }
-
-    public void extrage(int elem){
-        boolean found = false;
-        for (int i = 0; i < dim ; i++) {
-            if (date[i] == elem) {
-                found = true;
-            }
-        }
-        int i = 0;
-        while(i < dim && found == true){
-            if (date[i] == elem){
-                date[i] = 0;
-                n--;
-            }
-            i++;
-        }
-    }
-
-    public void afisare(){
-        for (int i = 0; i < dim; i++) {
-            System.out.print(date[i] + " ");
-        }
-        System.out.println();
-        System.out.println("Nr de elemente = " + n);
-    }
-
-}
+public class Multime {    private int[] date;    private int dim;    private int n;    public Multime(){        this.date = new int[dim];        this.n = 0;    }    public Multime(int dim){        this.date = new int[dim];        this.dim = dim;        this.n = 0;    }    public void adauga(int elem){        if (n != dim){            boolean found = false;            for (int i = 0; i < dim; i++) {                if (date[i] == elem){                    found = true;                }            }            if (found == false){                n++;                date[n-1] = elem;            }        } else {            System.out.println("Vectorul este plin.");        }    }    public void extrage(int elem){        for (int i = 0; i < dim ; i++) {            if (date[i] == elem){                for (int j = i; j < dim - 1; j++){                    date[j] = date[j + 1];                }                n--;            }        }    }    public void afisare(){        for (int i = 0; i < dim; i++) {            System.out.print(date[i] + " ");        }        System.out.println();        System.out.println("Nr de elemente = " + n);    }}
